038471bfda6dd18cd0f50d4d9cc1d21220ce89e8
Daniel <daniel@tea.xyz>
2024-07-29 06:39:45 +0200

    Add no transactions check

--- src/containers/accountDetailsPage/TransactionsTable.tsx
+++ src/containers/accountDetailsPage/TransactionsTable.tsx
@@ -39,52 +39,62 @@ const TransactionsTable: React.FC<Props> = ({ transactions }) => {
 
   return (
     <div className="overflow-x-auto">
-      <Typography size="xl" weight="bold">
-        Transaction history
-      </Typography>
-      <table className="min-w-[70vw] max-w-[80rem] divide-y divide-gray-200 mt-2 ">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Hash
-            </th>
-            <th
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
-              onClick={() => handleSort("value")}
-            >
-              Value {sortField === "value" && (sortOrder === "asc" ? "↑" : "↓")}
-            </th>
-            <th
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
-              onClick={() => handleSort("timestamp")}
-            >
-              Timestamp{" "}
-              {sortField === "timestamp" && (sortOrder === "asc" ? "↑" : "↓")}
-            </th>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
-          {sortedTransactions.map((tx) => (
-            <tr key={tx.hash}>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 max-w-[10rem] truncate">
-                <a
-                  href={`/transaction/${tx.hash}`}
-                  className="text-blue-600 hover:underline"
-                  title={tx.hash}
+      {transactions.length > 0 ? (
+        <>
+          <Typography size="xl" weight="bold">
+            Transaction history
+          </Typography>
+          <table className="min-w-[70vw] max-w-[80rem] divide-y divide-gray-200 mt-2 ">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Hash
+                </th>
+                <th
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+                  onClick={() => handleSort("value")}
                 >
-                  {tx.hash}
-                </a>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {formatWeiToEth(tx.value)}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {format(fromUnixTime(tx.timestamp), "PPpp")}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  Value{" "}
+                  {sortField === "value" && (sortOrder === "asc" ? "↑" : "↓")}
+                </th>
+                <th
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+                  onClick={() => handleSort("timestamp")}
+                >
+                  Timestamp{" "}
+                  {sortField === "timestamp" &&
+                    (sortOrder === "asc" ? "↑" : "↓")}
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {sortedTransactions.map((tx) => (
+                <tr key={tx.hash}>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 max-w-[10rem] truncate">
+                    <a
+                      href={`/transaction/${tx.hash}`}
+                      className="text-blue-600 hover:underline"
+                      title={tx.hash}
+                    >
+                      {tx.hash}
+                    </a>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {formatWeiToEth(tx.value)}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {format(fromUnixTime(tx.timestamp), "PPpp")}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <Typography size="xl" weight="bold">
+          No transactions found
+        </Typography>
+      )}
     </div>
   );
 };
